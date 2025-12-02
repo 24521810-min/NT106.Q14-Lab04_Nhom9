cf7e07d56beb537a825113fe6bca1ea647049a9b
24521719-max <24521719@ms.uit.edu.vn>
2025-12-02 17:58:54 +0700

    Giao dien dang ky bai 7

---
+++ Bai07/FrmSignup.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Bai07
+{
+    partial class FrmSignup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.grpUserInfo = new System.Windows.Forms.GroupBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblFirstname = new System.Windows.Forms.Label();
+            this.lblLastname = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblBirthday = new System.Windows.Forms.Label();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.lblLanguage = new System.Windows.Forms.Label();
+            this.cboLanguage = new System.Windows.Forms.ComboBox();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.radMale = new System.Windows.Forms.RadioButton();
+            this.radFemale = new System.Windows.Forms.RadioButton();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.grpUserInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG KÝ TÀI KHOẢN";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.lblPassword);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.lblUsername);
+            this.groupBox1.Location = new System.Drawing.Point(25, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(763, 155);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sign Up";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(32, 47);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(110, 25);
+            this.lblUsername.TabIndex = 0;
+            this.lblUsername.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(149, 40);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(571, 31);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(32, 94);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(106, 25);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(149, 94);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(571, 31);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // grpUserInfo
+            // 
+            this.grpUserInfo.Controls.Add(this.txtFirstname);
+            this.grpUserInfo.Controls.Add(this.txtLastname);
+            this.grpUserInfo.Controls.Add(this.txtPhone);
+            this.grpUserInfo.Controls.Add(this.radFemale);
+            this.grpUserInfo.Controls.Add(this.radMale);
+            this.grpUserInfo.Controls.Add(this.lblSex);
+            this.grpUserInfo.Controls.Add(this.cboLanguage);
+            this.grpUserInfo.Controls.Add(this.lblLanguage);
+            this.grpUserInfo.Controls.Add(this.dtpBirthday);
+            this.grpUserInfo.Controls.Add(this.lblBirthday);
+            this.grpUserInfo.Controls.Add(this.lblPhone);
+            this.grpUserInfo.Controls.Add(this.lblLastname);
+            this.grpUserInfo.Controls.Add(this.lblFirstname);
+            this.grpUserInfo.Controls.Add(this.txtEmail);
+            this.grpUserInfo.Controls.Add(this.lblEmail);
+            this.grpUserInfo.Location = new System.Drawing.Point(25, 228);
+            this.grpUserInfo.Name = "grpUserInfo";
+            this.grpUserInfo.Size = new System.Drawing.Size(763, 350);
+            this.grpUserInfo.TabIndex = 2;
+            this.grpUserInfo.TabStop = false;
+            this.grpUserInfo.Text = "User Information";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(21, 31);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(65, 25);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(92, 28);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(628, 31);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lblFirstname
+            // 
+            this.lblFirstname.AutoSize = true;
+            this.lblFirstname.Location = new System.Drawing.Point(21, 81);
+            this.lblFirstname.Name = "lblFirstname";
+            this.lblFirstname.Size = new System.Drawing.Size(107, 25);
+            this.lblFirstname.TabIndex = 2;
+            this.lblFirstname.Text = "Firstname";
+            // 
+            // lblLastname
+            // 
+            this.lblLastname.AutoSize = true;
+            this.lblLastname.Location = new System.Drawing.Point(21, 122);
+            this.lblLastname.Name = "lblLastname";
+            this.lblLastname.Size = new System.Drawing.Size(106, 25);
+            this.lblLastname.TabIndex = 3;
+            this.lblLastname.Text = "Lastname";
+            this.lblLastname.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(26, 162);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(74, 25);
+            this.lblPhone.TabIndex = 4;
+            this.lblPhone.Text = "Phone";
+            // 
+            // lblBirthday
+            // 
+            this.lblBirthday.AutoSize = true;
+            this.lblBirthday.Location = new System.Drawing.Point(21, 206);
+            this.lblBirthday.Name = "lblBirthday";
+            this.lblBirthday.Size = new System.Drawing.Size(91, 25);
+            this.lblBirthday.TabIndex = 5;
+            this.lblBirthday.Text = "Birthday";
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.Location = new System.Drawing.Point(134, 206);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(446, 31);
+            this.dtpBirthday.TabIndex = 6;
+            // 
+            // lblLanguage
+            // 
+            this.lblLanguage.AutoSize = true;
+            this.lblLanguage.Location = new System.Drawing.Point(21, 261);
+            this.lblLanguage.Name = "lblLanguage";
+            this.lblLanguage.Size = new System.Drawing.Size(108, 25);
+            this.lblLanguage.TabIndex = 7;
+            this.lblLanguage.Text = "Language";
+            // 
+            // cboLanguage
+            // 
+            this.cboLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboLanguage.FormattingEnabled = true;
+            this.cboLanguage.Items.AddRange(new object[] {
+            "vi,en"});
+            this.cboLanguage.Location = new System.Drawing.Point(132, 253);
+            this.cboLanguage.Name = "cboLanguage";
+            this.cboLanguage.Size = new System.Drawing.Size(392, 33);
+            this.cboLanguage.TabIndex = 0;
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(26, 306);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(49, 25);
+            this.lblSex.TabIndex = 8;
+            this.lblSex.Text = "Sex";
+            // 
+            // radMale
+            // 
+            this.radMale.AutoSize = true;
+            this.radMale.Location = new System.Drawing.Point(132, 304);
+            this.radMale.Name = "radMale";
+            this.radMale.Size = new System.Drawing.Size(90, 29);
+            this.radMale.TabIndex = 9;
+            this.radMale.Text = "Male";
+            this.radMale.UseVisualStyleBackColor = true;
+            // 
+            // radFemale
+            // 
+            this.radFemale.AutoSize = true;
+            this.radFemale.Location = new System.Drawing.Point(246, 304);
+            this.radFemale.Name = "radFemale";
+            this.radFemale.Size = new System.Drawing.Size(114, 29);
+            this.radFemale.TabIndex = 10;
+            this.radFemale.Text = "Female";
+            this.radFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(132, 159);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(586, 31);
+            this.txtPhone.TabIndex = 11;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(133, 122);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(585, 31);
+            this.txtLastname.TabIndex = 12;
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(134, 78);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(586, 31);
+            this.txtFirstname.TabIndex = 13;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnClear.Location = new System.Drawing.Point(399, 584);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(176, 42);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSubmit.Location = new System.Drawing.Point(581, 584);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(176, 42);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            // 
+            // FrmSignup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 638);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.grpUserInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.MinimizeBox = false;
+            this.Name = "FrmSignup";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng ký tài khoản";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.grpUserInfo.ResumeLayout(false);
+            this.grpUserInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.GroupBox grpUserInfo;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblBirthday;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.Label lblFirstname;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.ComboBox cboLanguage;
+        private System.Windows.Forms.Label lblLanguage;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.RadioButton radMale;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.RadioButton radFemale;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
