f81642cd8c471b11b86107bc199545a735ad4827
24521719-max <24521719@ms.uit.edu.vn>
2025-12-02 18:54:04 +0700

    Giao dien chinh, them mon an bai 7

---
+++ Bai07/FrmAddFood.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Bai07
+{
+    partial class FrmAddFood
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAddTitle = new System.Windows.Forms.Label();
+            this.lblFoodName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblImage = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtFoodName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtImageUrl = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAddTitle
+            // 
+            this.lblAddTitle.AutoSize = true;
+            this.lblAddTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddTitle.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblAddTitle.Location = new System.Drawing.Point(268, 9);
+            this.lblAddTitle.Name = "lblAddTitle";
+            this.lblAddTitle.Size = new System.Drawing.Size(258, 45);
+            this.lblAddTitle.TabIndex = 0;
+            this.lblAddTitle.Text = "THÊM MÓN ĂN";
+            // 
+            // lblFoodName
+            // 
+            this.lblFoodName.AutoSize = true;
+            this.lblFoodName.Font = new System.Drawing.Font("Segoe UI Semibold", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFoodName.Location = new System.Drawing.Point(21, 64);
+            this.lblFoodName.Name = "lblFoodName";
+            this.lblFoodName.Size = new System.Drawing.Size(130, 30);
+            this.lblFoodName.TabIndex = 1;
+            this.lblFoodName.Text = "Tên món ăn:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Segoe UI Semibold", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrice.Location = new System.Drawing.Point(102, 106);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(49, 30);
+            this.lblPrice.TabIndex = 2;
+            this.lblPrice.Text = "Giá:";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Segoe UI Semibold", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(69, 155);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(82, 30);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Địa chỉ:";
+            // 
+            // lblImage
+            // 
+            this.lblImage.AutoSize = true;
+            this.lblImage.Font = new System.Drawing.Font("Segoe UI Semibold", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImage.Location = new System.Drawing.Point(49, 214);
+            this.lblImage.Name = "lblImage";
+            this.lblImage.Size = new System.Drawing.Size(103, 30);
+            this.lblImage.TabIndex = 4;
+            this.lblImage.Text = "Hình ảnh:";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Segoe UI Semibold", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescription.Location = new System.Drawing.Point(81, 270);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(75, 30);
+            this.lblDescription.TabIndex = 5;
+            this.lblDescription.Text = "Mô tả:";
+            // 
+            // txtFoodName
+            // 
+            this.txtFoodName.Location = new System.Drawing.Point(159, 61);
+            this.txtFoodName.Name = "txtFoodName";
+            this.txtFoodName.Size = new System.Drawing.Size(597, 31);
+            this.txtFoodName.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(159, 106);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(597, 31);
+            this.txtPrice.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(159, 155);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(597, 31);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // txtImageUrl
+            // 
+            this.txtImageUrl.Location = new System.Drawing.Point(159, 211);
+            this.txtImageUrl.Name = "txtImageUrl";
+            this.txtImageUrl.Size = new System.Drawing.Size(597, 31);
+            this.txtImageUrl.TabIndex = 9;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(159, 264);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(597, 160);
+            this.txtDescription.TabIndex = 10;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(405, 431);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(180, 64);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(591, 431);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(180, 64);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // FrmAddFood
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 507);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtImageUrl);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtFoodName);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblImage);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblFoodName);
+            this.Controls.Add(this.lblAddTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "FrmAddFood";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Thêm món ăn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAddTitle;
+        private System.Windows.Forms.Label lblFoodName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblImage;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtFoodName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtImageUrl;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
---
+++ Bai07/FrmMain.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Bai07
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAnGiGio = new System.Windows.Forms.Button();
+            this.btnThemMon = new System.Windows.Forms.Button();
+            this.tabFoods = new System.Windows.Forms.TabControl();
+            this.tabAll = new System.Windows.Forms.TabPage();
+            this.tabMine = new System.Windows.Forms.TabPage();
+            this.flpAllFoods = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpMyFoods = new System.Windows.Forms.FlowLayoutPanel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblWelcome = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tslLogout = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panelPaging = new System.Windows.Forms.Panel();
+            this.lblPageText = new System.Windows.Forms.Label();
+            this.numPage = new System.Windows.Forms.NumericUpDown();
+            this.lblPageSizeText = new System.Windows.Forms.Label();
+            this.numPageSize = new System.Windows.Forms.NumericUpDown();
+            this.panelHeader.SuspendLayout();
+            this.tabFoods.SuspendLayout();
+            this.tabAll.SuspendLayout();
+            this.tabMine.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panelPaging.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPageSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Controls.Add(this.btnThemMon);
+            this.panelHeader.Controls.Add(this.btnAnGiGio);
+            this.panelHeader.Controls.Add(this.lblTitle);
+            this.panelHeader.Location = new System.Drawing.Point(12, 13);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(759, 106);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblTitle.Location = new System.Drawing.Point(3, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(239, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HÔM NAY ĂN GÌ?";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAnGiGio
+            // 
+            this.btnAnGiGio.BackColor = System.Drawing.SystemColors.Info;
+            this.btnAnGiGio.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnGiGio.Location = new System.Drawing.Point(366, 34);
+            this.btnAnGiGio.Name = "btnAnGiGio";
+            this.btnAnGiGio.Size = new System.Drawing.Size(190, 58);
+            this.btnAnGiGio.TabIndex = 1;
+            this.btnAnGiGio.Text = "Ăn gì giờ?";
+            this.btnAnGiGio.UseVisualStyleBackColor = false;
+            // 
+            // btnThemMon
+            // 
+            this.btnThemMon.BackColor = System.Drawing.SystemColors.Info;
+            this.btnThemMon.Font = new System.Drawing.Font("Segoe UI", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemMon.Location = new System.Drawing.Point(562, 34);
+            this.btnThemMon.Name = "btnThemMon";
+            this.btnThemMon.Size = new System.Drawing.Size(190, 58);
+            this.btnThemMon.TabIndex = 2;
+            this.btnThemMon.Text = "Thêm món ăn";
+            this.btnThemMon.UseVisualStyleBackColor = false;
+            // 
+            // tabFoods
+            // 
+            this.tabFoods.Controls.Add(this.tabAll);
+            this.tabFoods.Controls.Add(this.tabMine);
+            this.tabFoods.Location = new System.Drawing.Point(22, 125);
+            this.tabFoods.Name = "tabFoods";
+            this.tabFoods.SelectedIndex = 0;
+            this.tabFoods.Size = new System.Drawing.Size(766, 502);
+            this.tabFoods.TabIndex = 1;
+            // 
+            // tabAll
+            // 
+            this.tabAll.Controls.Add(this.flpAllFoods);
+            this.tabAll.Location = new System.Drawing.Point(8, 39);
+            this.tabAll.Name = "tabAll";
+            this.tabAll.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAll.Size = new System.Drawing.Size(750, 455);
+            this.tabAll.TabIndex = 0;
+            this.tabAll.Text = "All";
+            this.tabAll.UseVisualStyleBackColor = true;
+            // 
+            // tabMine
+            // 
+            this.tabMine.Controls.Add(this.flpMyFoods);
+            this.tabMine.Location = new System.Drawing.Point(8, 39);
+            this.tabMine.Name = "tabMine";
+            this.tabMine.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMine.Size = new System.Drawing.Size(750, 455);
+            this.tabMine.TabIndex = 1;
+            this.tabMine.Text = "Tôi đóng góp";
+            this.tabMine.UseVisualStyleBackColor = true;
+            // 
+            // flpAllFoods
+            // 
+            this.flpAllFoods.AutoScroll = true;
+            this.flpAllFoods.Location = new System.Drawing.Point(6, 6);
+            this.flpAllFoods.Name = "flpAllFoods";
+            this.flpAllFoods.Size = new System.Drawing.Size(728, 443);
+            this.flpAllFoods.TabIndex = 0;
+            // 
+            // flpMyFoods
+            // 
+            this.flpMyFoods.AutoScroll = true;
+            this.flpMyFoods.Location = new System.Drawing.Point(6, 6);
+            this.flpMyFoods.Name = "flpMyFoods";
+            this.flpMyFoods.Size = new System.Drawing.Size(735, 443);
+            this.flpMyFoods.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblWelcome,
+            this.tslLogout});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 674);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 42);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(113, 32);
+            this.lblWelcome.Text = "Welcome";
+            // 
+            // tslLogout
+            // 
+            this.tslLogout.IsLink = true;
+            this.tslLogout.Name = "tslLogout";
+            this.tslLogout.Size = new System.Drawing.Size(89, 32);
+            this.tslLogout.Text = "Logout";
+            // 
+            // panelPaging
+            // 
+            this.panelPaging.Controls.Add(this.numPageSize);
+            this.panelPaging.Controls.Add(this.lblPageSizeText);
+            this.panelPaging.Controls.Add(this.numPage);
+            this.panelPaging.Controls.Add(this.lblPageText);
+            this.panelPaging.Location = new System.Drawing.Point(12, 625);
+            this.panelPaging.Name = "panelPaging";
+            this.panelPaging.Size = new System.Drawing.Size(776, 46);
+            this.panelPaging.TabIndex = 3;
+            // 
+            // lblPageText
+            // 
+            this.lblPageText.AutoSize = true;
+            this.lblPageText.Location = new System.Drawing.Point(330, 12);
+            this.lblPageText.Name = "lblPageText";
+            this.lblPageText.Size = new System.Drawing.Size(62, 25);
+            this.lblPageText.TabIndex = 0;
+            this.lblPageText.Text = "Page";
+            // 
+            // numPage
+            // 
+            this.numPage.Location = new System.Drawing.Point(398, 6);
+            this.numPage.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPage.Name = "numPage";
+            this.numPage.Size = new System.Drawing.Size(120, 31);
+            this.numPage.TabIndex = 1;
+            this.numPage.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblPageSizeText
+            // 
+            this.lblPageSizeText.AutoSize = true;
+            this.lblPageSizeText.Location = new System.Drawing.Point(524, 8);
+            this.lblPageSizeText.Name = "lblPageSizeText";
+            this.lblPageSizeText.Size = new System.Drawing.Size(107, 25);
+            this.lblPageSizeText.TabIndex = 2;
+            this.lblPageSizeText.Text = "Page size";
+            // 
+            // numPageSize
+            // 
+            this.numPageSize.Location = new System.Drawing.Point(637, 6);
+            this.numPageSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPageSize.Name = "numPageSize";
+            this.numPageSize.Size = new System.Drawing.Size(120, 31);
+            this.numPageSize.TabIndex = 3;
+            this.numPageSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 716);
+            this.Controls.Add(this.panelPaging);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabFoods);
+            this.Controls.Add(this.panelHeader);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hôm nay ăn gì?";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.tabFoods.ResumeLayout(false);
+            this.tabAll.ResumeLayout(false);
+            this.tabMine.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panelPaging.ResumeLayout(false);
+            this.panelPaging.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPageSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnThemMon;
+        private System.Windows.Forms.Button btnAnGiGio;
+        private System.Windows.Forms.TabControl tabFoods;
+        private System.Windows.Forms.TabPage tabAll;
+        private System.Windows.Forms.FlowLayoutPanel flpAllFoods;
+        private System.Windows.Forms.TabPage tabMine;
+        private System.Windows.Forms.FlowLayoutPanel flpMyFoods;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblWelcome;
+        private System.Windows.Forms.ToolStripStatusLabel tslLogout;
+        private System.Windows.Forms.Panel panelPaging;
+        private System.Windows.Forms.Label lblPageText;
+        private System.Windows.Forms.NumericUpDown numPageSize;
+        private System.Windows.Forms.Label lblPageSizeText;
+        private System.Windows.Forms.NumericUpDown numPage;
+    }
+}
